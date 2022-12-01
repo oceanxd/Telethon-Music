e45d964c48740ea5c23e4d2e03e5ee029e3efaff
oceanxd <118508777+oceanxd@users.noreply.github.com>
2022-12-01 16:52:25 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "TDNMUSICBOT")
     SUPPORT = os.environ.get("SUPPORT", "TDN_CHAT") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "THE_DRAGON_NETWORK_OFFICIAL") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2bb51bb403336e3207ef7.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5486649568")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
